08d7682ac7448610e5f1e6050af6c51f22811c37
flocko <flo.kircher@gmail.com>
2015-11-26 19:57:35 +0100

    added page template

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package slouchy
+ */
+
+get_header(); ?>
+<div class="page-no-sidebar">
+  <main id="main" class="site-main" role="main">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+    <?php endwhile; // End of the loop. ?>
+
+  </main><!-- #main -->
+</div>
+
+<?php get_footer(); ?>
