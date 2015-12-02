4b1353e167bd05ba50b99c0af0913fa73fcf7893
flocko <flo.kircher@gmail.com>
2015-12-02 21:26:01 +0100

    update html

--- 404.php
+++ 404.php
@@ -9,50 +9,48 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'slouchy' ); ?></h1>
-				</header><!-- .page-header -->
+	<section class="error-404 not-found">
+		<header class="page-header">
+			<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'slouchy' ); ?></h1>
+		</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'slouchy' ); ?></p>
+		<div class="page-content">
+			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'slouchy' ); ?></p>
 
-					<?php get_search_form(); ?>
+			<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+			<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( slouchy_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'slouchy' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+			<?php if ( slouchy_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+			<div class="widget widget_categories">
+				<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'slouchy' ); ?></h2>
+				<ul>
+				<?php
+					wp_list_categories( array(
+						'orderby'    => 'count',
+						'order'      => 'DESC',
+						'show_count' => 1,
+						'title_li'   => '',
+						'number'     => 10,
+					) );
+				?>
+				</ul>
+			</div><!-- .widget -->
+			<?php endif; ?>
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'slouchy' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+			<?php
+				/* translators: %1$s: smiley */
+				$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'slouchy' ), convert_smilies( ':)' ) ) . '</p>';
+				the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+			?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+			<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+		</div><!-- .page-content -->
+	</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</main><!-- #main -->
 
 <?php get_footer(); ?>
